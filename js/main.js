51b3a09b301ddfd9c160f09a5f2f95d5d0c767b0
YahIsKing <hello@seekfirst.dev>
2025-01-16 17:40:59 -0600

    trying another

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 // Enable development logs first
 localStorage.setItem("show_ec_logs", "ALL");
 
+// Log script location
+console.log('Script location:', document.currentScript?.src || 'Unable to determine source');
+
+// Ensure we have the Ecwid object
+if (typeof Ecwid === 'undefined') {
+    console.error('Ecwid object not found');
+} else {
+    console.log('Ecwid object found');
+}
+
 // Basic test to see if our code is running
 console.log('Main.js loaded - testing Ecwid integration');
 
+// Main Ecwid integration
 Ecwid.OnAPILoaded.add(function() {
     console.log('Ecwid API loaded - test message');
     
     Ecwid.OnPageLoaded.add(function(page) {
-        console.log('Ecwid page loaded:', page.type);
+        console.log('Page loaded:', page.type);
         
         if (page.type == "PRODUCT") {
-            console.log('Product page detected:', {
-                storeId: Ecwid.getOwnerId(),
-                productId: page.productId
-            });
+            console.log('On product page:', page.productId);
+            
+            // Try to modify the page title as a test
+            document.title = 'Product: ' + page.name;
         }
     });
 });
