35c9494225ce76ccbaa0d7d9b63d37ff7f798b99
YahIsKing <hello@seekfirst.dev>
2025-01-16 17:11:09 -0600

    adding logging

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
+// Enable all development logs
+localStorage.setItem("show_ec_logs", "ALL");
+
 console.log('Testing - main.js file loaded');
 
 Ecwid.OnAPILoaded.add(function() {
-	Ecwid.OnPageLoaded.add(function(page) {
-    if (page.type == "PRODUCT") {
-       console.log(
-        `
-       Page loaded!
-       Ecwid store ID is: ${Ecwid.getOwnerId()}
-       Product ID is: ${page.productId}
-        `
-       )
-    }
-	})
-})
-// prints
-//
-//      Page loaded!
-//      Ecwid store ID is: 1003
-//      Product ID is: 560656065
+    console.log('API loaded');
+    
+    Ecwid.OnPageLoaded.add(function(page) {
+        console.log('Page loaded:', page.type);
+        
+        if (page.type == "PRODUCT") {
+            console.log(
+                `
+                Page loaded!
+                Ecwid store ID is: ${Ecwid.getOwnerId()}
+                Product ID is: ${page.productId}
+                `
+            )
+        }
+    });
+});
